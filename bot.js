6d68ee5aa57490d20de0ecaf6a1fb8def7202d50
Qusaiiii <31171895+Qusaiiii@users.noreply.github.com>
2018-11-08 18:33:48 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,8 +1,11 @@
 var ServerID = "496001281643773993";// اي دي الروم
 var ChannelID = "510128205336805386";
+var ServerIDD = "496001281643773993";
+var ChannelIDD = "510128205336805386";
 
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const c = new Discord.Client();
 const prefix = '!';
 
 client.on('warn', console.warn);
@@ -66,6 +69,68 @@ client.user.setAvatar(`https://cdn.discordapp.com/avatars/324672376455299074/524
 }
 });
 
+C.on('warn', console.warn);
+
+C.on('error', console.error);
+
+
+
+C.on('ready', () => console.log('ProBot Credits Miner Discord.js Script'));
+
+C.on('disconnect', () => console.log('PROBOT credits miner had disconnected!'));
+
+C.on('reconnecting', () => console.log('PROBOT credits miner is reconnecting...'));
+
+
+function timerFunc() {
+    c.on('message', msg => {
+        c.guilds.get(ServerIDD).channels.get(ChannelIDD).send(Math.random().toString(36).substring(7))
+
+
+    });
+}
+
+c.on('message', message => {
+
+  if (message.author.bot) return;
+
+  if (!message.content.startsWith(prefix)) return;
+
+
+
+  let command = message.content.split(" ")[0];
+
+  command = command.slice(prefix.length);
+
+
+
+let args = message.content.split(" ").slice(1);
+
+if(!args) return message.channel.send('${prefix}say <words>');	
+
+
+
+if (command == "say") {
+
+if(!message.guild.member(message.author).hasPermission("ADMINISTRATOR")) return message.reply("*لا تملك الصلاحيات المطلوبه**");
+
+
+
+message.channel.send(args.join("  "))
+
+    message.delete();
+
+  }
+
+});
+c.on('message', message => {
+     if (message.content === "!av-set") {
+c.user.setAvatar(`https://cdn.discordapp.com/avatars/324672376455299074/524312009e5ce435a1adf99080f76e63.jpg`)
+ 
+}
+});
+
 var timer = setTimeout(timerFunc, 1000);
 
 client.login(process.env.BOT_TOKEN); 
+c.login(process.env.TOKEN);
